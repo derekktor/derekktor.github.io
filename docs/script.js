b7b2b456f3b5dc3ad51c8d40fe98e8f1354c9e06
Derek <baldka1109bh@gmail.com>
2022-08-15 12:31:23 +0200

    other resources

---
+++ docs/script.js
@@ -0,0 +1,30 @@
+let theme = localStorage.getItem("theme")
+
+if (theme == null) {
+	setTheme("light")
+} else {
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName("theme-dot")
+
+for (var i=0; themeDots.length > i; i++) {
+	themeDots[i].addEventListener("click", function(){
+		let mode = this.dataset.mode
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode) {
+	// console.log("mode: ", mode)
+
+	if (mode == "light") {
+		document.getElementById("theme-style").href = "default.css"
+	} 
+
+	if (mode == "dark") {
+		document.getElementById("theme-style").href = "dark.css"
+	} 
+
+	localStorage.setItem("theme", mode)
+}
